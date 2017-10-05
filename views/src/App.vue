86a8b525ead09a48bd4616d48068a0aaec3e1a54
fedwatch <fed-watch@foxmail.com>
2017-10-05 19:52:07 +0800

    u

--- views/src/App.vue
+++ views/src/App.vue
@@ -105,3 +105,21 @@
         opacity: 0;
     }
 </style>
+
+
+<script>
+    import $ from 'jQuery';
+    export default {
+        ready(){
+            $.ajaxSetup({ cache: true });
+            $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
+                FB.init({
+                    appId: '{your-app-id}',
+                    version: 'v2.7' // or v2.1, v2.2, v2.3, ...
+                });
+                $('#loginbutton,#feedbutton').removeAttr('disabled');
+                FB.getLoginStatus(updateStatusCallback);
+            });
+        }
+    }
+</script>
